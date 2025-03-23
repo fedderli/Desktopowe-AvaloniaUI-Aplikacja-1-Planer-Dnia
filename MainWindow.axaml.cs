180d7507a0c7889d3b89301de46c889fb0b01f5d
moje <twojemail@example.com>
2025-03-23 17:41:35 +0100

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Desktopowe_AvaloniaUI_Aplikacja_1_Planer_Dnia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
